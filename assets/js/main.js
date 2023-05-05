2ded3dee2fbeb6901a265e7059bb20b146f457ec
Ândrei <andrei8246@gmail.com>
2023-05-05 10:29:37 -0300

    tabs dos stats e abilities

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,39 +9,83 @@ const maxRecords = 151;
 function loadPokemons(offset, limit) {
   pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
     const newHtml = pokemons.map((pokemon) => `
-    <li class="pokemon ${pokemon.type}">
-          <span class="number">#${pokemon.number}</span>
-          <span class="name">${pokemon.name}</span>
+      <li class="pokemon ${pokemon.type}">
+        <span class="number">#${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
 
-          <div class="detail">
-              <ol class="types">
-                  ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-              </ol>
-              <img src="${pokemon.picture}"
-                  alt="${pokemon.name}">
-          </div>
-          <div class="pokemonskills">
-            <div class="stats">
-              <h4>Stats</h4>
-              <ul>
-              ${pokemon.stats.map((stats => `<li>${stats.stat.name}: ${stats.base_stat}</li>`)).join('')}
-              </ul>
-            </div>
-            <div class="abilities">
-              <h4>Abilities</h4>
-                <ul>
-                  ${pokemon.abilities.map((slot) => `<li>${slot.name}</li>`).join('')}
-                </ul>
-            </div>
+        <div class="detail">
+          <ol class="types">
+            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+          </ol>
+          <img src="${pokemon.picture}" alt="${pokemon.name}">
+        </div>
 
-          </div>
+        <div id="pokemon-${pokemon.id}" class="tab">
+        <button class="tablinks active" onclick="toggleTabs(event, 'stats')">Stats</button>
+        <button class="tablinks" onclick="toggleTabs(event, 'abilities')">Abilities</button>
+        
+  
+  <div id="stats" class="tabcontent">
+    <h4>Stats</h4>
+    <ul>
+      ${pokemon.stats.map((stats => `<li>${stats.stat.name}: ${stats.base_stat}</li>`)).join('')}
+    </ul>
+  </div>
+  
+  <div id="abilities" class="tabcontent" style="display: none;">
+    <h4>Abilities</h4>
+    <ul>
+      ${pokemon.abilities.map((slot) => `<li>${slot.name}</li>`).join('')}
+    </ul>
+  </div>
+</div>
       </li>
-  `).join('')
+    `).join('');
     pokemonLista.innerHTML += newHtml;
 
-  })
+    // Adiciona o evento de clique nas abas
+    const tabs = document.querySelectorAll('.tab');
+    tabs.forEach((tab) => {
+      tab.addEventListener('click', (event) => {
+        // Esconde todas as abas e conteúdos
+        const tabContents = document.querySelectorAll('.tab-content');
+        tabContents.forEach((tabContent) => {
+          tabContent.classList.remove('active');
+        });
+        tabs.forEach((tab) => {
+          tab.classList.remove('active');
+        });
 
+        // Mostra a aba e o conteúdo clicados
+        const tabClicked = event.target;
+        const tabContent = document.querySelector(`.tab-content[data-tab="${tabClicked.dataset.tab}"]`);
+        tabClicked.classList.add('active');
+        tabContent.classList.add('active');
+      });
+    });
+  });
 }
+
+function toggleTabs(event, tabName) {
+  const pokemonTab = event.currentTarget.parentNode;
+  const tabcontent = pokemonTab.querySelectorAll('.tabcontent');
+  const tablinks = pokemonTab.querySelectorAll('.tablinks');
+
+  tabcontent.forEach((tab) => {
+    tab.style.display = 'none';
+  });
+
+  tablinks.forEach((tab) => {
+    tab.className = tab.className.replace(' active', '');
+  });
+
+  const tab = pokemonTab.querySelector(`#${tabName}`);
+  tab.style.display = 'block';
+
+  event.currentTarget.className += ' active';
+}
+
+
 
 loadPokemons(offset, limit);
 
